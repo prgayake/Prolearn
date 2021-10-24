807fb52643f54c37feb2949784b202efdd093c9c
Mayur Ghodekar <70889731+mayur-23@users.noreply.github.com>
2021-10-25 01:19:23 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
-/**
-* Template Name: FlexStart - v1.7.0
-* Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+  
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -19,9 +12,7 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
+  
   const on = (type, el, listener, all = false) => {
     if (all) {
       select(el, all).forEach(e => e.addEventListener(type, listener))
@@ -30,16 +21,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+ 
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+  
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -57,9 +44,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
+ 
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -75,9 +60,7 @@
     })
   }
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
+ 
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
@@ -91,9 +74,7 @@
     onscroll(document, headerScrolled)
   }
 
-  /**
-   * Back to top button
-   */
+  
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -107,18 +88,14 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
+  
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Mobile nav dropdowns activate
-   */
+  
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
@@ -126,9 +103,7 @@
     }
   }, true)
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+  
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -144,9 +119,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
+  
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -155,9 +128,7 @@
     }
   });
 
-  /**
-   * Clients Slider
-   */
+  
   new Swiper('.clients-slider', {
     speed: 400,
     loop: true,
@@ -191,9 +162,7 @@
     }
   });
 
-  /**
-   * Porfolio isotope and filter
-   */
+
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -220,16 +189,12 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
+  
   const portfolioLightbox = GLightbox({
     selector: '.portfokio-lightbox'
   });
 
-  /**
-   * Portfolio details slider
-   */
+
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     autoplay: {
@@ -243,9 +208,7 @@
     }
   });
 
-  /**
-   * Testimonials slider
-   */
+ 
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -271,9 +234,7 @@
     }
   });
 
-  /**
-   * Animation on scroll
-   */
+  
   function aos_init() {
     AOS.init({
       duration: 1000,
